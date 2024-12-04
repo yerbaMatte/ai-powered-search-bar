b5168a060b2f6e69997af45083b9fc21abeb26e0
yerbaMatte <milosz.lewandowski@icloud.com>
2024-12-04 17:26:49 +0100

    create layout components and css styles

---
+++ components/layout/ThemeSwticher/ThemeSwitcher.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+
+const ThemeSwitcher = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+      Change mode
+    </button>
+  );
+};
+
+export default ThemeSwitcher;
