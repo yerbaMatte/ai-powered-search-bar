bed10f64671b88612ae16452b6139a4e1be3d441
yerbaMatte <milosz.lewandowski@icloud.com>
2024-12-05 13:09:43 +0100

    add styles to layout components

--- components/layout/Footer/Footer.tsx
+++ components/layout/Footer/Footer.tsx
@@ -1,7 +1,25 @@
 import styles from "./Footer.module.scss";
+import Image from "next/image";
 
 const Footer = () => {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <div className={styles.footer}>
+      <a
+        href="https://www.blog.yerbamatte.com"
+        target="_blank"
+        className={styles.anchor}
+      >
+        <Image
+          src="/mate.svg"
+          alt="yerba mate drink"
+          width={24}
+          height={24}
+          className={styles.mate}
+        />
+        <span className="font-bold"> blog.yerbamatte.com</span>
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
