691438337497e0f6d772d7b5b9f434722c5e85ec
yerbaMatte <milosz.lewandowski@icloud.com>
2024-12-05 13:15:04 +0100

    add suggestions component with mock data

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,31 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export const suggestionsData = [
+  "apple",
+  "banana",
+  "blueberry",
+  "cherry",
+  "grape",
+  "mango",
+  "orange",
+  "pineapple",
+  "strawberry",
+  "watermelon",
+];
+
+export async function fetchSuggestions(query) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(
+        suggestionsData.filter((item) =>
+          item.toLowerCase().startsWith(query.toLowerCase())
+        )
+      );
+    }, 200);
+  });
 }
